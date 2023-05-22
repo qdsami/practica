24afdb25b37e0af8ce946bc9967ea1252ca2c1a5
qdsami <47449943+qdsami@users.noreply.github.com>
2023-05-22 11:04:56 +0600

    Add files via upload

---
+++ alimzhan11last/alimzhan11last/src/main/java/com/example/alimzhan11last/models/Image.java
@@ -0,0 +1,53 @@
+package com.example.alimzhan11last.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "originalFileName")
+    private String originalFileName;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "contentType")
+    private String contentType;
+
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+}
---
+++ alimzhan11last/alimzhan11last/src/main/java/com/example/alimzhan11last/repositories/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.example.alimzhan11last.repositories;
+
+import com.example.alimzhan11last.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
